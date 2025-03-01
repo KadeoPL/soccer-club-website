ff75abd7838ecd20f6abc679476013e39b829462
KadeoPL <stopkakrzysztof1@gmail.com>
2025-03-01 11:54:46 +0100

    add router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,18 @@
+import logo from "@/assets/gdovia_logo.png";
+import { NavLink } from "react-router";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <NavLink to="/">
+          <img
+            className="h-[70px] md:h-[115px]"
+            src={logo}
+            alt="Herb drużyny Gdovia Gdów"
+          />
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
