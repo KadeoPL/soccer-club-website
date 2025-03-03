702d9aa38bc24081ba75532ff4c947fb873cf1b4
KadeoPL <stopkakrzysztof1@gmail.com>
2025-03-03 23:34:07 +0100

    Add Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,30 @@
-import logo from "@/assets/gdovia_logo.png";
+import logo from "../assets/gdovia_logo.png";
 import { NavLink } from "react-router";
 
 export default function Navbar() {
   return (
     <nav>
-      <div>
+      <div className="flex md:flex-row justify-between md:items-center md:w-[1260px] h-[120px] w-full mx-auto">
         <NavLink to="/">
           <img
-            className="h-[70px] md:h-[115px]"
+            className="h-[70px] md:h-[100px]"
             src={logo}
-            alt="Herb drużyny Gdovia Gdów"
+            alt="Herb drużyny"
           />
         </NavLink>
+        <div className="h-full flex items-center">
+          <ul className="flex gap-10 text-lg uppercase font-semibold text-main">
+            <li className="">
+              <NavLink to="/">Aktualności</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Seniorzy</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Szkółka piłkarska</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
