1129253417352b84a02f6ecfa02e93937ccac301
KadeoPL <stopkakrzysztof1@gmail.com>
2025-03-04 22:31:03 +0100

    create hover for md size Navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div className="w-full h-[800px] bg-gray-500"></div>;
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,33 @@
 import logo from "../assets/gdovia_logo.png";
 import { NavLink } from "react-router";
 
+const menuItems = [
+  {
+    label: "Aktualności",
+    link: "/aktualnosci",
+  },
+  {
+    label: "Seniorzy",
+    link: "/seniorzy",
+  },
+  {
+    label: "Szkółka piłkarska",
+    link: "/szkolka-pilkarska",
+  },
+  {
+    label: "Multimedia",
+    link: "/multimedia",
+  },
+  {
+    label: "Kontakt",
+    link: "/kontakt",
+  },
+];
+
 export default function Navbar() {
   return (
     <nav>
-      <div className="flex md:flex-row justify-between md:items-center md:w-[1260px] h-[120px] w-full mx-auto">
+      <div className="flex md:flex-row justify-between md:items-center md:max-w-[1260px] h-[120px] w-full mx-auto px-2.5">
         <NavLink to="/">
           <img
             className="h-[70px] md:h-[100px]"
@@ -13,16 +36,15 @@ export default function Navbar() {
           />
         </NavLink>
         <div className="h-full flex items-center">
-          <ul className="flex gap-10 text-lg uppercase font-semibold text-main">
-            <li className="">
-              <NavLink to="/">Aktualności</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">Seniorzy</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">Szkółka piłkarska</NavLink>
-            </li>
+          <ul className="flex gap-10 text-lg text-main h-full items-center">
+            {menuItems.map((item, index) => (
+              <li
+                key={index}
+                className="h-full flex items-center hover:cursor-pointer hover:border-b-4 duration-150 ease-in"
+              >
+                <NavLink to={item.link}>{item.label}</NavLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
