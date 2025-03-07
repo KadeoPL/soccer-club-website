e7f72df4fc93e86cb1363b17bd5787796cd3492d
KadeoPL <stopkakrzysztof1@gmail.com>
2025-03-07 23:36:35 +0100

    add styles to small resolution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,17 +38,17 @@ export default function Header() {
   };
   return (
     <div
-      className="w-full h-[800px]"
+      className="w-full lg:h-[800px] h-[600px] bg-center bg-cover"
       style={{ backgroundImage: `url(${headers[activeIndex].image})` }}
     >
       <div className="w-full h-full bg-gradient-to-t from-black to-black/0">
-        <div className="flex items-end justify-between h-full max-w-[1260px] mx-auto px-2.5">
-          <h1 className=" w-1/2 text-5xl text-white font-black mb-20 cursor-pointer hover:text-gray-300 duration-300 ease-in-out">
+        <div className="flex lg:flex-row flex-col lg:items-end items-center lg:justify-between justify-end h-full max-w-[1260px] mx-auto px-2.5">
+          <h1 className=" lg:w-1/2 w-full lg:text-5xl text-4xl text-center lg:text-left text-white font-black lg:mb-20 cursor-pointer hover:text-gray-300 duration-300 ease-in-out">
             <NavLink to={headers[activeIndex].link}>
               {headers[activeIndex].text}
             </NavLink>
           </h1>
-          <div className="w-26 h-12 mb-20 flex justify-between">
+          <div className="w-26 h-12 lg:mb-20 mg:mt-0 mb-10 mt-10  flex justify-between">
             <div
               className="grid w-12 h-12 bg-primary place-items-center hover:bg-secondary duration-150 ease-in-out cursor-pointer"
               onClick={prevSlide}
